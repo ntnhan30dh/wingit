eb3792c6deb230a762517261d00b12449558b843
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2020-09-14 18:02:17 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div className="menu">
+        <ul>
+          <li>Our Story</li>
+          <li>Menu</li>
+          <li>Follow Us</li>
+          <li>Contact</li>
+          <li>Oder Now</li>
+        </ul>
+      </div>
+      <div className="ourStory"></div>
+      <div className="menu"></div>
+      <div className="contact"></div>
+      <div className="followUs"></div>
+    </div>
+  )
 }
